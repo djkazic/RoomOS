4adca1f34566b8017f80aa2873d818780d62da30
Kevin Cai <kevincai96@gmail.com>
2015-06-25 15:12:48 -0400

    Rearranged filter boolean logic for PersonalizedModule

--- src/main/java/org/djkazic/RoomOS/basemodules/PersonalizedModule.java
+++ src/main/java/org/djkazic/RoomOS/basemodules/PersonalizedModule.java
@@ -5,23 +5,16 @@ import org.djkazic.RoomOS.Utils;
 
 public abstract class PersonalizedModule extends Module {
 	
-	private Utils uc;
-	private boolean loggedIn;
-	
 	public PersonalizedModule(String trigger) {
 		super(trigger);
-		uc = new Utils();
-	}
-
-	public boolean filter(String cmd) {
-		boolean basicMatch = super.filter(cmd);
-		if(loggedIn = RTCore.getCurrentProfile() == null) {
-			uc.speak("No current profile is logged in.");
-		}
-		return (basicMatch && !loggedIn);
 	}
 	
 	public boolean getIndependentBoolean() {
-		return loggedIn;
+		Utils uc = new Utils();
+		boolean indep = RTCore.getCurrentProfile() != null;
+		if(!indep) {
+			uc.speak("I require profile data for this action, please log in.");
+		}
+		return indep;
 	}
 }
