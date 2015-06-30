fdcb777fb19cca0d2d5b711207942cf9a77fbfe2
Kevin Cai <kevincai96@gmail.com>
2015-06-30 14:26:05 -0400

    Added icon hook for AmbienceModule + remove debugging

--- src/main/java/org/djkazic/RoomOS/modules/AmbienceModule.java
+++ src/main/java/org/djkazic/RoomOS/modules/AmbienceModule.java
@@ -2,6 +2,7 @@ package org.djkazic.RoomOS.modules;
 
 import org.djkazic.RoomOS.RTCore;
 import org.djkazic.RoomOS.basemodules.Module;
+import org.djkazic.RoomOS.util.Settings;
 import org.djkazic.RoomOS.util.Utils;
 
 public class AmbienceModule extends Module {
@@ -14,18 +15,22 @@ public class AmbienceModule extends Module {
 	}
 	
 	public void process() {
+		if(Settings.gui) {
+			RTCore.getWindow().setLoop("authenticate");
+		}
+		
 		Utils uc = new Utils();
 		if(rt.ambientListening && rule.equals("cmd_ambient_deactivate")) {
 			uc.speak("Disabling ambient listening");
 			rt.ambientListening = false;
-			latch.countDown(); //Go back to the main thread, where this will be instantiated again
+			latch.countDown();
 			return;
 		}
 		
 		if(!rt.ambientListening && rule.equals("cmd_ambient_activate")) {
 			uc.speak("Enabling ambient listening");
 			rt.ambientListening = true;
-			latch.countDown(); //Go back to the main thread, where normal behavior resumes
+			latch.countDown();
 			return;
 		}
 		latch.countDown();
