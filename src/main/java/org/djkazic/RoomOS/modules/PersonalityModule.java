b019e455790f6da5fda8db0e98cbd31a8d112b72
Kevin Cai <kevincai96@gmail.com>
2015-07-03 12:44:44 -0400

    Conversion of RFetcher to module

---
+++ src/main/java/org/djkazic/RoomOS/modules/PersonalityModule.java
@@ -0,0 +1,18 @@
+package org.djkazic.RoomOS.modules;
+
+import org.djkazic.RoomOS.basemodules.Module;
+import org.djkazic.RoomOS.sql.ResponseFetcher;
+import org.djkazic.RoomOS.util.Utils;
+
+public class PersonalityModule extends Module {
+
+	public PersonalityModule() {
+		super("personality_*");
+	}
+
+	public void process() {
+		Utils uc = new Utils();
+		ResponseFetcher rf = new ResponseFetcher();
+		uc.speak(rf.queryForRule(rule));
+	}
+}
